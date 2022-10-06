1281acaf5fbfc5b03c312e9aae44c28c7ca9d003
Quantata <ceoahn95@gmail.com>
2022-10-06 10:34:56 +0900

    2022 10 06 by nyahn - Ch8 : Fragment-Activity 연결

--- criminalintent/src/main/java/com/nancone/criminalintent/ui/MainActivity.kt
+++ criminalintent/src/main/java/com/nancone/criminalintent/ui/MainActivity.kt
@@ -8,5 +8,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // FragmentManager 에 Fragment 를 관리하도록 넘겨주는 코드
+        val currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
+
+        if(currentFragment == null) {
+            val fragment = CrimeFragment()
+            // fragment transaction 을 생성하고 커밋함.
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragment_container, fragment)
+                .commit()
+        }
     }
 }
