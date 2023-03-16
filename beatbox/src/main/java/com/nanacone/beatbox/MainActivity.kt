2980f883f8c0465ca3826e0941ba49c23850c811
nyahn <nyahn@markany.com>
2023-03-16 15:11:35 +0900

    feat: 2022 03 16 by nyahn - CH20: Challenge: SeekBar UI 구현

--- beatbox/src/main/java/com/nanacone/beatbox/MainActivity.kt
+++ beatbox/src/main/java/com/nanacone/beatbox/MainActivity.kt
@@ -3,6 +3,7 @@ package com.nanacone.beatbox
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.ViewGroup
+import android.widget.SeekBar
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -12,6 +13,7 @@ import com.nanacone.beatbox.databinding.ListItemSoundBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var beatBox: BeatBox
+    private lateinit var soundViewModel: SoundViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,18 +28,55 @@ class MainActivity : AppCompatActivity() {
             layoutManager = GridLayoutManager(context, 3)
             adapter = SoundAdapter(beatBox.sounds)
         }
+
+        binding.seekbar.progress = 5
+        binding.seekbarTitle.text = String.format(getString(R.string.seekbar_title), binding.seekbar.progress/10.0)
+
+
+        binding.seekbar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) { // seekbar 조작하고 있는 중에 작동
+                if(progress <= 5) {
+                    seekBar?.progress = 5 // 5 이하로는 안떨어지게
+                }
+                seekBar?.progress =
+                    if(progress % 5 == 0) progress
+                    else if(progress % 10 < 5) (progress/10) * 10 // progress 는 5 = 0.5 / 12 -> 1 * 10 = 10
+                    else (progress/10) * 10 + 5 // 17 -> 1*10 + 5 = 15
+
+                binding.seekbarTitle.text  = String.format(getString(R.string.seekbar_title), seekBar?.progress!!/10.0)
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) { // seekbar 조작이 시작했을 때 작동
+//                seekBar?.progress =
+//                    if(seekBar?.progress!! % 10 < 5) (seekBar.progress/10) * 10 // progress 는 5 = 0.5 / 12 -> 1 * 10 = 10
+//                    else (seekBar.progress/10) * 15 // 17 -> 1 * 15 = 15
+
+                // text 설정
+                binding.seekbarTitle.text = String.format(getString(R.string.seekbar_title), "${(seekBar?.progress ?: 5)/10.0}")
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) { // seekbar 조작이 끝났을 때 작동
+                // text 설정
+                binding.seekbarTitle.text = String.format(getString(R.string.seekbar_title), "${(seekBar?.progress ?: 10)/10.0}")
+//                soundViewModel.sound?.let { beatBox.play(sound = it, seekBar?.progress) }
+
+            }
+        })
     }
 
     override fun onDestroy() {
         super.onDestroy()
         beatBox.release()
     }
+
     // list_item_sound.xml 과 연결
     private inner class SoundHolder(private val binding: ListItemSoundBinding) :
             RecyclerView.ViewHolder(binding.root) {
                 init {
                     // list_item_sound.xml 에 viewmodel <data> 선언으로 ListItemSoundBinding 에서 viewmodel 을 속성으로 가짐
-                    binding.viewModel = SoundViewModel(beatBox) // 초기화
+                    soundViewModel = SoundViewModel(beatBox)
+//                    binding.viewModel = SoundViewModel(beatBox) // 초기화
+                    binding.viewModel = soundViewModel // 초기화
 
                 }
 
